025d78caed84ac9c3749a37a5149db96a4a931c7
Nergaard <nergaard14@gmail.com>
2019-07-25 12:01:29 +0200

    Changes

--- starttkinter.py
+++ starttkinter.py
@@ -1,19 +1,12 @@
 from tkinter import *
 
 
-window = Toplevel()
-window.attributes('-fullscreen', True)
-window.title("Butikkomaten")
-#.withdraw()
-#window.configure(background='white')
 
-photo=PhotoImage(file="button_start.png")
-photo2=PhotoImage(file="button_send-inn.png")
-
-#top_frame = tkinter.Frame(window).pack()
-#bottom_frame = tkinter.Frame(window).pack(side = "bottom")
-
-#funksjon
+def send_item():
+    print("Seems to work")
+    #Write code to send information to database
+    # ---- #
+    last_page()
 
 def create_Window():
     window2 = Toplevel()
@@ -32,30 +25,38 @@ def create_Window():
     knapp2.place(relx=0.5, rely=0.8, anchor = CENTER)
     window.destroy()
 
-def send_item():
-    print("Seems to work")
-    #Write code to send information to database
-    # ---- #
-    last_page()
+
 
 def last_page():
     window3 = Toplevel()
     window3.attributes('-fullscreen', True)
     window3.title("Butikkomaten")
 
-    ny = Label(window2, text = "Oensker du aa legge til et nytt ønske?",font=(None, 25))
+    ny = Label(window3, text = "Oensker du aa legge til et nytt ønske?",font=(None, 25))
     ny.place(relx=0.5, rely=0.3, anchor = CENTER)
-    knapp2 = Button(window2, image = photo2, border= 0, command=send_item)
-    knapp2.place(relx=0.5, rely=0.8, anchor = CENTER)
-    window.destroy()
+    ja = Button(window3, text = "JA", border= 0, command=first_window)
+    ja.place(relx=0.5, rely=0.8, anchor = CENTER)
 
 
+def first_window():
+    window = Toplevel()
+    window.attributes('-fullscreen', True)
+    window.title("Butikkomaten")
+    knapp = Button(window, image = photo, border= 0, command=create_Window)
+    #knapp.bind("<Button-1>",say_hi)
+    knapp.place(relx=0.5, rely=0.5, anchor = CENTER)
+    #generisk button - #btn2 = tkinter.Button(top_frame, text = "Button2", fg = "black", bg = "white").pack()
+    #.withdraw()
+    #window.configure(background='white')
 
+
+window = Toplevel()
+window.attributes('-fullscreen', True)
+window.title("Butikkomaten")
+photo=PhotoImage(file="button_start.png")
 knapp = Button(window, image = photo, border= 0, command=create_Window)
 #knapp.bind("<Button-1>",say_hi)
 knapp.place(relx=0.5, rely=0.5, anchor = CENTER)
-#generisk button - #btn2 = tkinter.Button(top_frame, text = "Button2", fg = "black", bg = "white").pack()
-
-
+photo2=PhotoImage(file="button_send-inn.png")
 
 window.mainloop()
