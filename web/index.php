027dab47a8e0530cca4881f3073b71619a27985b
Nergaard <nergaard14@gmail.com>
2019-07-30 07:54:51 +0200

    Web GUI

---
+++ web/index.php
@@ -0,0 +1,13 @@
+#Including the neccessary pages; DB-connection and head.
+<?php
+	include_once('include/pdo_con_inc.php');
+	include_once('modules/head.php');
+?>
+
+#This is the main wrapper. This has a reference to the file include. Include dictates the content that will be shown inside
+#the wrapper.
+<main class="main_wrapper">
+		<?php include('include/include.php'); ?>
+</main>
+
+<?php include_once('pages/footer.php'); ?>
---
+++ web/modules/head.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Customate</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+		#Change---
+    <link href="https://fonts.googleapis.com/css?family=Oswald:500|Roboto:300,300i,400|Josefin+Sans:100,300|Raleway:300,400,700" rel="stylesheet">
+	</head>
+
+	<body>
