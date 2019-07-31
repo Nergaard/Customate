bf4afe9a9ffda7f0f57610b5e8117080a9dcbf42
Nergaard <nergaard14@gmail.com>
2019-07-31 15:58:45 +0200

    frame 3

--- web/pages/store_overview.php
+++ web/pages/store_overview.php
@@ -3,22 +3,35 @@
     <section class="header">
       <div class="grid-container2">
         <p id="logohead">CUSTOMATE</p>
-        <a id="logginn" href="#">LOGG INN</a>
+        <a id="logginn" href="#">LOGG UT</a>
       </div>
     </section>
   </div>
   <!-- This is how the list should be constructed, but you need to write a php-method to fetch the data and build the list-->
   <ol class="centered_list">
     <li class= "odd">Cola Zero 1,5 liter</li>
-    <li >Cola Zero 1,5 liter</li>
+    <li class="even">Cola Zero 1,5 liter</li>
     <li class= "odd">Cola Zero 1,5 liter</li>
-    <li>Cola Zero 1,5 liter</li>
+    <li class="even">Cola Zero 1,5 liter</li>
     <li class= "odd">Cola Zero 1,5 liter</li>
-    <li>Cola Zero 1,5 liter</li>
+    <li class="even">Cola Zero 1,5 liter</li>
     <li class= "odd">Cola Zero 1,5 liter</li>
-    <li>Cola Zero 1,5 liter</li>
+    <li class="even">Cola Zero 1,5 liter</li>
     <li class= "odd">Cola Zero 1,5 liter</li>
-    <li>Cola Zero 1,5 liter</li>
+    <li class="even">Cola Zero 1,5 liter</li>
   </ol>
+
+  <ul class="next_list">
+    <li class= "nlist">120</li>
+    <li class= "nlist2">112</li>
+    <li class= "nlist3">101</li>
+    <li class= "nlist4">80</li>
+    <li class= "nlist5">67</li>
+    <li class= "nlist6">45</li>
+    <li class= "nlist7">34</li>
+    <li class= "nlist8">20</li>
+    <li class= "nlist9">12</li>
+    <li class= "nlist10">3</li>
+</ul>
 
 </div>
