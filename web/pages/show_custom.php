dae716ea735efc25870cf2e24b80651568a728dc
Nergaard <nergaard14@gmail.com>
2019-08-01 12:11:29 +0200

    frame 4

--- web/pages/show_custom.php
+++ web/pages/show_custom.php
@@ -19,6 +19,6 @@
                 <div class="card"> <div class="card_img"> </div> <div class="card_descript"> Forenkle tilbakemeldinger fra kunder! <br> Customate leverer automater <br> som gir kundene dine en <br> sømløs måte å gi din <br> virsomhet tilbakemeldinger. </div> </div>
                   <div class="card"> <div class="card_img"> </div> <div class="card_descript"> Forenkle tilbakemeldinger fra kunder! <br> Customate leverer automater <br> som gir kundene dine en <br> sømløs måte å gi din <br> virsomhet tilbakemeldinger. </div> </div>
 </div>
-  <a id="left-scrolly" type="button"><</a>
-  <a id="right-scrolly" type="button">></a>
+  <a id="left-scrolly" type="button"> <img src="/img/left.png" alt=""> </a>
+  <a id="right-scrolly" type="button"> <img src="/img/right.png" alt=""> </a>
 </div>
